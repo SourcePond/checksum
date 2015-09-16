88b9d6c0c8f3a3a2e8c5ebe96a15bb04159eee13
Roland Hauser <holyroli82@gmail.com>
2015-09-16 18:46:37 +0200

    Added test

---
+++ src/test/java/ch/sourcepond/utils/checksum/impl/ChecksumFactoryActivatorTest.java
@@ -0,0 +1,47 @@
+package ch.sourcepond.utils.checksum.impl;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+import org.junit.Test;
+import org.osgi.framework.BundleContext;
+
+import ch.sourcepond.utils.checksum.ChecksumFactory;
+
+/**
+ * @author rolandhauser
+ *
+ */
+public class ChecksumFactoryActivatorTest {
+	private final BundleContext context = mock(BundleContext.class);
+	private final ChecksumFactory factory = mock(ChecksumFactory.class);
+	private final ChecksumFactoryActivator activator = new ChecksumFactoryActivator(factory);
+
+	/**
+	 * 
+	 */
+	@Test
+	public void verifyDefaultConstructor() {
+		// Should not cause an exception
+		new ChecksumFactoryActivator();
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	@Test
+	public void verifyStart() throws Exception {
+		activator.start(context);
+		verify(context).registerService(ChecksumFactory.class, factory, null);
+	}
+
+	/**
+	 * @throws Exception
+	 */
+	@Test
+	public void verifyStop() throws Exception {
+		activator.stop(context);
+		verifyZeroInteractions(context);
+	}
+}
