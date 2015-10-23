8b63405f382aecd6632aee8c35bcb859a349c4aa
Roland Hauser <holyroli82@gmail.com>
2015-10-23 15:20:42 +0200

    Added license

--- checksum-impl/src/main/java/ch/sourcepond/io/checksum/impl/digest/DigestHelper.java
+++ checksum-impl/src/main/java/ch/sourcepond/io/checksum/impl/digest/DigestHelper.java
@@ -1,3 +1,16 @@
+/*Copyright (C) 2015 Roland Hauser, <sourcepond@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.*/
 package ch.sourcepond.io.checksum.impl.digest;
 
 import static ch.sourcepond.io.checksum.impl.DefaultChecksumBuilderFactory.DEFAULT_BUFFER_SIZE;
@@ -18,7 +31,6 @@ import java.security.MessageDigest;
 import org.slf4j.Logger;
 
 /**
- * @author rolandhauser
  *
  */
 final class DigestHelper {
